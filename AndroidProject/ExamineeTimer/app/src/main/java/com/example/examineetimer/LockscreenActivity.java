e491aaca0ede4022c9c915e6014de4f2e79c29ec
OfficialDominyellow <dominyellow@gmail.com>
2020-02-21 23:56:36 +0900

    밀어서 잠금해제
    를 만들긴 했는데.. 뭔가 시발 잘못된 방향인거 같은데

--- AndroidProject/ExamineeTimer/app/src/main/java/com/example/examineetimer/LockscreenActivity.java
+++ AndroidProject/ExamineeTimer/app/src/main/java/com/example/examineetimer/LockscreenActivity.java
@@ -3,17 +3,51 @@ package com.example.examineetimer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.ncorti.slidetoact.SlideToActView;
 
 public class LockscreenActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lockscreen);
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+        SlideToActView sta = (SlideToActView) findViewById(R.id.sld_unlock);
+        sta.setOnSlideCompleteListener(new SlideToActView.OnSlideCompleteListener() {
+            @Override
+            public void onSlideComplete(SlideToActView slideToActView) {
+                //Send Timer
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+        int newUiOptions = uiOptions;
+        boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+        if (isImmersiveModeEnabled) {
+            Log.i("Is on?", "Turning immersive mode mode off.");
+        } else {
+            Log.i("Is on?", "Turning immersive mode mode on.");
+        }
+
+        //하단바 없애려면 필수 각
+        newUiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        newUiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
     }
 }
